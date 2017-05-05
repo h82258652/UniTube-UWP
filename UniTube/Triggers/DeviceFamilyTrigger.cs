3f264e3fc2a0ca61148af139f8752b650c948e20
nahuelale <nahual_gomca@outlook.com.ar>
2017-05-04 21:15:10 -0300

    Added DeviceFamilyTrigger

---
+++ UniTube/Triggers/DeviceFamilyTrigger.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace UniTube.Triggers
+{
+    /// <summary>
+    /// Trigger for switching between Windows and Windows Phone
+    /// </summary>
+    public class DeviceFamilyTrigger : StateTriggerBase, ITriggerValue
+    {
+        private static string deviceFamily;
+
+        static DeviceFamilyTrigger()
+        {
+            deviceFamily = Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily;
+        }
+
+        public DeviceFamilyTrigger()
+        {
+
+        }
+
+        public DeviceFamily DeviceFamily
+        {
+            get { return (DeviceFamily)GetValue(DeviceFamilyProperty); }
+            set { SetValue(DeviceFamilyProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceFamilyProperty = DependencyProperty.Register(
+            nameof(DeviceFamily), typeof(DeviceFamily), typeof(DeviceFamilyTrigger), new PropertyMetadata(DeviceFamily.Unknown, OnDeviceTypePropertyChanged));
+
+        private static void OnDeviceTypePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (DeviceFamilyTrigger)d;
+            var val = (DeviceFamily)e.NewValue;
+            if (deviceFamily == "Windows.Mobile")
+                obj.IsActive = (val == DeviceFamily.Mobile);
+            else if (deviceFamily == "Windows.Desktop")
+                obj.IsActive = (val == DeviceFamily.Desktop);
+            else if (deviceFamily == "Windows.Team")
+                obj.IsActive = (val == DeviceFamily.Team);
+            else if (deviceFamily == "Windows.IoT")
+                obj.IsActive = (val == DeviceFamily.IoT);
+            else if (deviceFamily == "Windows.Holographic")
+                obj.IsActive = (val == DeviceFamily.Holographic);
+            else if (deviceFamily == "Windows.Xbox")
+                obj.IsActive = (val == DeviceFamily.Xbox);
+            else
+                obj.IsActive = (val == DeviceFamily.Unknown);
+            
+        }
+
+        #region ITriggerValue
+
+        private bool m_IsActive;
+
+        public bool IsActive
+        {
+            get { return m_IsActive; }
+            private set
+            {
+                if (m_IsActive != value)
+                {
+                    m_IsActive = value;
+                    SetActive(value);
+                    IsActiveChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler IsActiveChanged;
+
+        #endregion ITriggerValue
+    }
+
+    public enum DeviceFamily
+    {
+        Unknown = 0,
+        Desktop = 1,
+        Mobile = 2,
+        Team = 3,
+        IoT = 4,
+        Xbox = 5,
+        Holographic = 6
+    }
+}
---
+++ UniTube/Triggers/ITriggerValue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UniTube.Triggers
+{
+    public interface ITriggerValue
+    {
+        bool IsActive { get; }
+        event EventHandler IsActiveChanged;
+    }
+}
