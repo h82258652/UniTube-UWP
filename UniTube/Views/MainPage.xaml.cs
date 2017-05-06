534ab82905b4d842656de0d218d0fd1c20395108
nahuelale <nahual_gomca@outlook.com.ar>
2017-05-06 13:40:35 -0300

    Removed unused code and improved code alignments

--- UniTube/Views/MainPage.xaml.cs
+++ UniTube/Views/MainPage.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using Microsoft.Graphics.Canvas.Effects;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+
 using System.Numerics;
-using System.Runtime.InteropServices.WindowsRuntime;
+
 using Windows.ApplicationModel.Core;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Graphics.Effects;
 using Windows.UI.Composition;
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Hosting;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,9 +18,9 @@ namespace UniTube.Views
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        Compositor _compositor;
-        SpriteVisual _contentSprite;
-        SpriteVisual _paneSprite;
+        Compositor      _compositor;
+        SpriteVisual    _contentSprite;
+        SpriteVisual    _paneSprite;
 
         public MainPage()
         {
@@ -41,11 +30,11 @@ namespace UniTube.Views
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            _contentSprite = _compositor.CreateSpriteVisual();
-            _paneSprite = _compositor.CreateSpriteVisual();
+            _contentSprite  = _compositor.CreateSpriteVisual();
+            _paneSprite     = _compositor.CreateSpriteVisual();
 
             _contentSprite.Size = new Vector2((float)ContentBackground.ActualWidth, (float)ContentBackground.ActualHeight);
-            _paneSprite.Size = new Vector2((float)PaneBackground.ActualWidth, (float)PaneBackground.ActualHeight);
+            _paneSprite.Size    = new Vector2((float)PaneBackground.ActualWidth, (float)PaneBackground.ActualHeight);
 
             ElementCompositionPreview.SetElementChildVisual(ContentBackground, _contentSprite);
             ElementCompositionPreview.SetElementChildVisual(PaneBackground, _paneSprite);
@@ -62,11 +51,11 @@ namespace UniTube.Views
             IGraphicsEffect graphicsEffect = new ColorMatrixEffect
             {
                 ColorMatrix = mat,
-                Source = new CompositionEffectSourceParameter("ImageSource")
+                Source      = new CompositionEffectSourceParameter("ImageSource")
             };
 
             CompositionEffectFactory _effectFactory = _compositor.CreateEffectFactory(graphicsEffect, null);
-            CompositionEffectBrush brush = _effectFactory.CreateBrush();
+            CompositionEffectBrush brush            = _effectFactory.CreateBrush();
 
             brush.SetSourceParameter("ImageSource", _compositor.CreateHostBackdropBrush());
 
@@ -75,27 +64,27 @@ namespace UniTube.Views
             CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             coreTitleBar.ExtendViewIntoTitleBar = true;
 
-            TitleBar.Height = coreTitleBar.Height;
-            PaneTitleBar.Height = coreTitleBar.Height;
-            ContentTitleBar.Height = coreTitleBar.Height;
+            TitleBar.Height         = coreTitleBar.Height;
+            PaneTitleBar.Height     = coreTitleBar.Height;
+            ContentTitleBar.Height  = coreTitleBar.Height;
             Window.Current.SetTitleBar(TitleBar);
 
-            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
-            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+            coreTitleBar.LayoutMetricsChanged   += CoreTitleBar_LayoutMetricsChanged;
+            coreTitleBar.IsVisibleChanged       += CoreTitleBar_IsVisibleChanged;
         }
 
         private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
         {
-            TitleBar.Visibility = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
-            PaneTitleBar.Visibility = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
-            ContentTitleBar.Visibility = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            TitleBar.Visibility         = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            PaneTitleBar.Visibility     = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            ContentTitleBar.Visibility  = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
         }
 
         private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
         {
-            TitleBar.Height = sender.Height;
-            PaneTitleBar.Height = sender.Height;
-            ContentTitleBar.Height = sender.Height;
+            TitleBar.Height         = sender.Height;
+            PaneTitleBar.Height     = sender.Height;
+            ContentTitleBar.Height  = sender.Height;
         }
 
         private void ContentBackground_SizeChanged(object sender, SizeChangedEventArgs e)
